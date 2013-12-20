cc0d76d4ad7dd3980d0d9b0f3b1541865867c4b8
Frede1012 <frederik.bolding@gmail.com>
2013-12-20 17:47:02 +0100

    FPS Counter + Fixes

---
+++ Scripts/FPS.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS : MonoBehaviour
+{
+	
+		public bool show = true;
+	
+		//FPS
+		public GUIStyle fpsStyle;
+		float updateInterval = 0.5F;
+		float fps;
+		private float accum = 0;
+		private int frames = 0;
+		private float timeleft;
+		string output;
+	
+		void Start ()
+		{
+				timeleft = updateInterval;
+		}
+	
+		void Update ()
+		{
+				if (show == true) {
+						timeleft -= Time.deltaTime;
+						accum += Time.timeScale / Time.deltaTime;
+						++frames;
+			
+						if (timeleft <= 0.0) {
+								fps = accum / frames;
+								float fpsFloat = 0.00F;
+								fpsFloat = fps;
+								output = "FPS: " + fpsFloat.ToString ("0.00");
+								timeleft = updateInterval;
+								accum = 0.0F;
+								frames = 0;
+						}
+				}
+		}
+	
+		void OnGUI ()
+		{
+				GUI.depth = -2000;
+				if (show == true) {
+						if (fps > 30) {
+								fpsStyle.normal.textColor = Color.green;
+						} else if (fps < 30) {
+								fpsStyle.normal.textColor = Color.yellow;
+						} else if (fps < 10) {
+								fpsStyle.normal.textColor = Color.red;
+						}
+
+			
+						float outputWidth = fpsStyle.CalcSize (new GUIContent (output)).x;
+						GUI.Label (new Rect (Screen.width - outputWidth, 0, 100, 50), output, fpsStyle);
+				}
+
+		}
+}
+
--- Scripts/Visualizer.cs
+++ Scripts/Visualizer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Visualizer : MonoBehaviour
 {
@@ -16,7 +17,9 @@ public class Visualizer : MonoBehaviour
 				for (int i = 0; i<1023; i++) {
 						lines++;
 						lineRenderers [i] = (GameObject)GameObject.Instantiate (line);
-						lineRenderers [i].SetActive (true);
+						if (i != 0) {
+								lineRenderers [i].SetActive (true);
+						}
 						lineRenderers [i].name = "Line " + lines;
 						lineRenderers [i].transform.parent = line.transform.parent;
 						lineRenderers [i].GetComponent<LineRenderer> ().material = new Material (shader);
@@ -27,23 +30,35 @@ public class Visualizer : MonoBehaviour
 
 		void Update ()
 		{
-				float[] spectrum = source.GetSpectrumData (1024, 0, FFTWindow.BlackmanHarris);
-				for (int i = 1; i<1023; i++) {
-						Debug.DrawLine (new Vector3 (i - 1, Mathf.Log (spectrum [i - 1]) + 10, 2), new Vector3 (i, Mathf.Log (spectrum [i]) + 10, 2), Color.cyan);
-						lineRenderers [i].GetComponent<LineRenderer> ().SetPosition (0, new Vector3 (i - 1, Mathf.Log (spectrum [i - 1]) + 10, 2));
-						lineRenderers [i].GetComponent<LineRenderer> ().SetPosition (1, new Vector3 (i, Mathf.Log (spectrum [i]) + 10, 2));
+				if (source.isPlaying == true) {
+						float[] spectrum = source.GetSpectrumData (1024, 0, FFTWindow.BlackmanHarris);
+						for (int i = 1; i<1023; i++) {
+								Debug.DrawLine (new Vector3 (i - 1, Mathf.Log (spectrum [i - 1]) + 10, 2), new Vector3 (i, Mathf.Log (spectrum [i]) + 10, 2), Color.cyan);
+								lineRenderers [i].GetComponent<LineRenderer> ().SetPosition (0, new Vector3 (i - 1, Mathf.Log (spectrum [i - 1]) + 10, 2));
+								lineRenderers [i].GetComponent<LineRenderer> ().SetPosition (1, new Vector3 (i, Mathf.Log (spectrum [i]) + 10, 2));
+						}
+				} else {
+						for (int i = 1; i<1023; i++) {
+								Debug.DrawLine (new Vector3 (0, 10, 2), new Vector3 (0, 10, 2), Color.cyan);
+								lineRenderers [i].GetComponent<LineRenderer> ().SetPosition (0, new Vector3 (i - 1, 0, 2));
+								lineRenderers [i].GetComponent<LineRenderer> ().SetPosition (1, new Vector3 (i, 0, 2));
+						}
 				}
 		}
 
 		void OnGUI ()
 		{
-				if (GUI.Button (new Rect (0, 0, 50, 20), "Play")) {
+				GUI.Label (new Rect (0, 0, 200, 100), "Name: " + source.clip.name);
+				TimeSpan ts = TimeSpan.FromSeconds ((source.clip.length - source.time));
+				string timeLeft = ts.Minutes.ToString ("00") + ":" + ts.Seconds.ToString ("00");
+				GUI.Label (new Rect (0, 20, 200, 100), "Time Left: " + timeLeft);
+				if (GUI.Button (new Rect (0, 40, 50, 20), "Play")) {
 						if (source.isPlaying == false) {
 								source.Play ();
 						}
 				}
-
-				if (GUI.Button (new Rect (0, 20, 50, 20), "Pause")) {
+		
+				if (GUI.Button (new Rect (0, 60, 50, 20), "Pause")) {
 						if (source.isPlaying == true) {
 								source.Pause ();
 						}
